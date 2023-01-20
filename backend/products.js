b82095df9325293feff15a51cf94dc6d9721b343
mueez sanwo <mueezsanwo@gmail.com>
2023-01-20 04:28:40 +0100

    2nd commit

---
+++ backend/products.js
@@ -0,0 +1,51 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Onions",
+        "info": "Fresh farm picked onions ",
+        "img": "https://th.bing.com/th/id/OIP.CTzPCyt9hXj1tBhrrCXqMwHaFO?w=262&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+  
+      {
+        "id": 2,
+        "name": "Cucumber",
+        "info": "Fresh farm picked cucumber ",
+        "img": "https://th.bing.com/th/id/OIP.zMkZdQ-oZXsRxcwmryQNJgHaE-?w=264&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+  
+      {
+        "id": 3,
+        "name": "Spinach",
+        "info": "Fresh farm picked spinach ",
+        "img": "https://th.bing.com/th/id/OIP.gkeybU5b2VYV_Z1DORbuUgHaE7?w=303&h=202&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+  
+      {
+        "id": 4,
+        "name": "Ginger",
+        "info": "Fresh farm picked ginger ",
+        "img": "https://th.bing.com/th/id/OIP.n0cBO2LmjdDR-3WXwNBfUwHaEn?w=265&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+  
+      {
+        "id": 5,
+        "name": "Carrot",
+        "info": "Fresh farm picked carrots ",
+        "img": "https://th.bing.com/th/id/OIP.dFtcgYnmXrjM-vOhSgiIOgHaFa?w=269&h=189&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+  
+      {
+        "id": 6,
+        "name": "Cabbage",
+        "info": "Fresh farm picked cabbage ",
+        "img": "https://th.bing.com/th/id/OIP.yXmaYnxErCMR1HJpCB5ouwHaHa?w=188&h=188&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+        "price": "10" 
+      },
+    ];
+
+    module.exports = products;
